58c9763bcbc141a8067ea05836279b1cfba10a67
yujc <jch_yu@hotmail.com>
2015-01-17 10:39:42 +0800

    提交原始代码

---
+++ Application/Common/Conf/performance.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    /* 性能设置 */
+    'TMPL_CACHE_ON'=>false, // 模板缓存
+    'HTML_CACHE_ON'=>false, // 静态缓存
+    'DB_SQL_BUILD_CACHE'=>false, // SQL查询缓存
+    'URL_ROUTER_ON'=>true, // URL路由
+    'URL_MODEL'=>3, // URL访问模式
+);
