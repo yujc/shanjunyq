005cb01695d07f41300b759a2391677d36a698f9
yujc <jch_yu@hotmail.com>
2015-01-17 10:31:38 +0800

    源代码嵌入

---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,25 @@
+<?php
+$dir = dirname(__FILE__);
+//载入其他配置(//解决入口无法定义配置的问题)
+$performance = include $dir.'/performance.php';
+$shield = include $dir.'/shield.php';
+$db = include $dir.'/db.php';
+$ver = include $dir.'/ver.php';
+$config = array(
+	//'配置项'=>'配置值'
+	'TMPL_ENGINE_TYPE' => 'Dux',
+    'TMPL_TEMPLATE_SUFFIX'=>'.html',
+    'VAR_SESSION_ID' => 'session_id',
+    'URL_HTML_SUFFIX'=>'html',
+	'SITE_URL' => '',
+	'APP_SYSTEM' => 1,
+	'APP_NAME' => '基础应用',
+	'URL_CASE_INSENSITIVE' => false, 
+	);
+
+$config = array_merge($performance,$shield,$db,$ver,$config,(array)$GLOBALS['config']);
+//设置404页面
+if(!$config['SHOW_ERROR_MSG']){
+	$config['TMPL_EXCEPTION_FILE'] = ROOT_PATH.'Themes/Common/404.html';
+}
+return $config;
