328a068721758e0b28f1e229571b3e4c8ddbda5e
yujc <jch_yu@hotmail.com>
2015-02-10 08:20:24 +0800

    第一阶段

--- Application/Common/Conf/shield.php
+++ Application/Common/Conf/shield.php
@@ -4,6 +4,6 @@ return array(
     'LOG_RECORD'=>false, // 日志记录
     'SHOW_ERROR_MSG'=>true, // 出错显示
     'DB_SQL_LOG'=>false, // SQL日志
-    'COOKIE_PREFIX'=>'GI03q_', // COOKIE前缀
-    'SAFE_KEY'=>'5bB5dcGr2od5CTJAqkzb', // 加密码
+    'COOKIE_PREFIX'=>'ujKoB_', // COOKIE前缀
+    'SAFE_KEY'=>'C5kHycYe16X4BQ5SKxka', // 加密码
 );
